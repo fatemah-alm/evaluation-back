b406e0e054bdbce18855b2f06a054e6283e0e64b
fatemah-alm <fatemah.alm@hotmail.com>
2022-04-18 13:06:03 +0300

    fix semesters serializer to show projects

--- semesters/serializers.py
+++ semesters/serializers.py
@@ -11,9 +11,16 @@ class UserSerializer(serializers.ModelSerializer):
         
 
 class SemesterListSerializer(serializers.ModelSerializer):
+    projects = serializers.SerializerMethodField(method_name='getProjects')
+
     class Meta:
         model = Semester
         fields = '__all__'
+    def getProjects(self,project:Project):
+        semester = []
+        for project in project.project_set.all():
+            semester.append(project.name)
+        return semester
         
         
 class SemesterDetailSerializer(serializers.ModelSerializer):
