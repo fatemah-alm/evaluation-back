87a38025c877ac5a6773c7e386846d81831a44d4
fatemah-alm <fatemah.alm@hotmail.com>
2022-04-21 17:31:01 +0300

    model migrations

--- criteria/models.py
+++ criteria/models.py
@@ -10,7 +10,6 @@ from django.db import models
 class Criteria(models.Model):
     name = models.CharField(max_length=120)
     weight = models.IntegerField()
-    isSelected = models.BooleanField(default=False)
     def __str__(self):
         return self.name
     
